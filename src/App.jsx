31b618a1c12da927d129ebad2250cd2984f43b63
Ritik Raushan <ritikraushan5941>
2026-08-27 13:15:09 +0530

    Fix GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
 import Products from "./pages/Products";
-
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
 
@@ -14,17 +13,18 @@ export default function App() {
   return (
     <div className="app-shell">
       <Navbar />
+
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
           <Route path="/products" element={<Products />} />
-       
           <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
+
       <Footer />
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,38 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./styles/global.css";
+import { Routes, Route } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Products from "./pages/Products";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
+
+export default function App() {
+  return (
+    <div className="app-shell">
+      <Navbar />
+
+      <main>
+        <Routes>
+          {/* HOME */}
+          <Route path="/" element={<Home />} />
+
+          {/* OTHER PAGES */}
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/contact" element={<Contact />} />
+
+          {/* 404 */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
